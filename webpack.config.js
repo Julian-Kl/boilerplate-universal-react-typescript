ffd0e7f00d85ba408594822227c756684cdc516b
Julian-Kl <40470983+Julian-Kl@users.noreply.github.com>
2021-04-27 15:32:42 +0200

    linters & prettier setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,57 @@
-const path = require('path');
+const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
 const clientConfig = {
-  entry: './src/client/index.tsx',
-  mode: 'production',
-  target: 'web',
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        include: [path.resolve(__dirname, 'src')],
-        use: 'ts-loader',
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    publicPath: 'public',
-    filename: 'client.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-};
+    entry: './src/client/index.tsx',
+    mode: 'production',
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        publicPath: 'public',
+        filename: 'client.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+}
 
 const serverConfig = {
-  entry: './src/server/index.tsx',
-  // target: 'node',
-  externals: [nodeExternals()],
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        include: [path.resolve(__dirname, 'src')],
-        use: 'ts-loader',
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    publicPath: 'public',
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    compress: true,
-    hot: true,
-    port: 8080,
-  }
-};
+    entry: './src/server/index.tsx',
+    // target: 'node',
+    externals: [nodeExternals()],
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        publicPath: 'public',
+        filename: 'server.js',
+        path: path.resolve(__dirname + 'public'),
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+}
 
 module.exports = [clientConfig, serverConfig]
