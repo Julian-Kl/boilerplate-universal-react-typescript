d69444fc50b00e678cff0a47461c961ac5a90edb
Julian-Kl <40470983+Julian-Kl@users.noreply.github.com>
2021-04-28 16:13:52 +0200

    added react helmet

--- src/app/routes/route1/Subpage1.tsx
+++ src/app/routes/route1/Subpage1.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 export const Subpage1: React.FC = () => {
-    return <p>Subpage1</p>
+    return (
+        <>
+            <Helmet>
+                <title>Subpage1</title>
+                <meta name='description' content='Subpage1' />
+                <meta property='og:title' content='Subpage1' />
+            </Helmet>
+            <p>Startseite</p>
+        </>
+    )
 }
--- src/server.tsx
+++ src/server.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import express from 'express'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import { html } from './app/html'
 import { App } from './app/App'
 
@@ -10,6 +11,8 @@ const server = express()
 
 server.use(express.static('public'))
 
+const helmet = Helmet.renderStatic()
+
 server.get('*', (req, res) => {
     const body = renderToString(
         <StaticRouter location={req.url}>
@@ -17,7 +20,7 @@ server.get('*', (req, res) => {
         </StaticRouter>
     )
 
-    res.send(html(body))
+    res.send(html(helmet, body))
 })
 
 server.listen(port, () => console.log(`App listening on port ${port}!`))
